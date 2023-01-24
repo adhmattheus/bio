569a7002e1ee41b6743a019452fe631fd1e5d2f3
adhmattheus <mattheusadhonnaysf@hotmail.com>
2023-01-24 11:27:15 -0300

    finalizando aplicação

---
+++ index.js
@@ -0,0 +1,13 @@
+function changeTheme() {
+  const html = document.documentElement;
+  html.classList.toggle('light');
+
+  const img = document.querySelector('#profile img')
+
+  if (html.classList.contains('light')) {
+    img.setAttribute("src", "./assets/avatar_light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+};
